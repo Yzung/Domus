7d9391246e90e3d55234aa980713dbc2c2c2b2a4
Yzung <wagnner_carlesso@outlook.com.br>
2025-04-17 17:35:58 -0300

    simple update

--- src/modules/users/routes/user_route.ts
+++ src/modules/users/routes/user_route.ts
@@ -1,16 +1,3 @@
-import { Router } from 'express';
-import { userController } from '../controllers/user_controller';
+import { request } from "express";
 
-const router = Router();
-
-/**
- * @swagger
- * /usuarios:
- *   get:
- *     summary: Retorna todos os usuários
- *     responses:
- *       200:
- *         description: Lista de usuários
- */
-
-export default router;
+require
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,17 @@
 import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from '../docs/swagger';
-import user_route from "./modules/users/routes/user_route"
+
 
 dotenv.config();
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
-const HOST = process.env.DB_HOST || '192.168.163.41';
+const HOST = process.env.DB_HOST || 'localhost';
 
-app.use(cors());
-app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use('/users', user_route);
 
-app.listen(PORT, '192.168.163.41', () => {
+app.listen(PORT, 'localhost', () => {
   console.log(`✅ Servidor rodando na porta ${PORT}`);
 });
